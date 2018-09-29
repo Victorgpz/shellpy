dd484a39a0abfde7a1314e5c434f4c97872f3d9c
Victor <35284183+Victorgpz@users.noreply.github.com>
2018-09-29 15:28:06 +0530

    Added

--- core.py
+++ core.py
@@ -8,5 +8,23 @@ os.system('apt update && apt upgrade')
 os.system('apt install nmap')
 print '###### Done'
 print "###### Type 'nmap' to start."
-time.sleep(10)
+time.sleep(1)
+
+print '\n###### Installing RED HAWK'
+os.system('apt update && apt upgrade')
+os.system('apt install git php')
+os.system('git clone https://github.com/Tuhinshubhra/RED_HAWK')
+os.system('mv RED_HAWK ~')
+print '###### Done'
+time.sleep(.500)
+
+print '\n###### Installing D-Tect'
+os.system('apt update && apt upgrade')
+os.system('apt install python2 git')
+os.system('git clone https://github.com/shawarkhanethicalhacker/D-TECT')
+os.system('mv D-TECT ~')
+print '###### Done' 
+time.sleep(.500)
+
+
 
