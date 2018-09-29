368ec984116804442851a5f16435a381c43b4cac
Victor <35284183+Victorgpz@users.noreply.github.com>
2018-09-29 16:10:33 +0530

    Ade

--- core.py
+++ core.py
@@ -18,12 +18,58 @@ os.system('mv RED_HAWK ~')
 print '###### Done'
 time.sleep(1)
 
-print '\n###### Installing D-Tect'
+print '\n###### Installing sqlmap'
+os.system('apt update && apt upgrade')
+os.system('apt install git python2')
+os.system('git clone https://github.com/sqlmapproject/sqlmap')
+os.system('mv sqlmap ~')
+print '###### Done'
+backtomenu_option()
+time.sleep(1)
+
+
+print '\n###### Installing Infoga'
 os.system('apt update && apt upgrade')
 os.system('apt install python2 git')
-os.system('git clone https://github.com/shawarkhanethicalhacker/D-TECT')
-os.system('mv D-TECT ~')
+os.system('pip2 install requests urllib3 urlparse')
+os.system('git clone https://github.com/m4ll0k/Infoga')
+os.system('mv Infoga ~')
 print '###### Done'
+time.sleep(1)
+
+print '\n###### Installing ReconDog'
+os.system('apt update && apt upgrade')
+os.system('apt install python2 git')
+os.system('git clone https://github.com/UltimateHackers/ReconDog')
+os.system('mv ReconDog ~')
+print '###### Done'
+time.sleep(1)
+
+print '\n###### Installing AndroZenmap'
+os.system('apt update && apt upgrade')
+os.system('apt install nmap curl')
+os.system('curl -O http://override.waper.co/files/androzenmap.txt')
+os.system('mkdir ~/AndroZenmap')
+os.system('mv androzenmap.txt ~/AndroZenmap/androzenmap.sh')
+print '###### Done'
+time.sleep(1)
+
+print '\n###### Installing WTF'
+os.system('apt update && apt upgrade')
+os.system('apt install git python2')
+os.system('pip2 bs4 requests HTMLParser urlparse mechanize argparse')
+os.system('git clone https://github.com/Xi4u7/wtf')
+os.system('mv wtf ~')
+print '###### Done'
+time.sleep(1)
+
+
+
+
+
+
+
+
 
 
 
